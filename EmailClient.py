61e9ee9ee86a0fb582daf3ad1937a9a1c31b5ac2
joewadk <jkabir000@citymail.cuny.edu>
2024-07-20 23:57:51 -0400

    implemented csv scraping+ firebase

--- EmailClient.py
+++ EmailClient.py
@@ -2,7 +2,9 @@ from socket import *
 from smtplib import SMTP_SSL
 from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
 load_dotenv()
+app=FastAPI()
 mailserver= "smtp.gmail.com" #server host we will be using
 serverPort=465 #port the socket uses (think of localhost:5000 when deploying a webapp)
 username='acmbyte.ccny@gmail.com' 
@@ -10,13 +12,13 @@ password= os.getenv('app_password')#this is the app password, you will have to g
 message_body=input('Enter message:\n')
 header= 'Subject: ' + input('Enter subject:\n')
 message=f"{header}\n\n{message_body}"
-recipient=input('Enter a recipient: \n')
+recipient=input('Enter a recipient: \n') 
 with SMTP_SSL(mailserver, serverPort) as server: #at this point, TCP has been established.
     try:
         server.ehlo() #identifier to esmtp server
         #print(server.verify(mailserver))   
         server.login(username,password) #login handler
-        server.sendmail(username,recipient,message)
+        server.sendmail(username,recipient,message)#note: recipient input for the sendmail function can be a list, as long as we append a ', ' to each, we can push to as many emails.
         server.quit() #close tcp
         print("inshallah we send")
     except:
