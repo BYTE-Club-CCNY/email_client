9d7e5547e3fc2cbe94b68900a86e052d1b75ac26
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-08-25 15:21:03 -0400

    adding people database

---
+++ database/people.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS people (
+    uid UUID NOT NULL DEFAULT uuid_generate_v4(),
+    active boolean NOT NULL DEFAULT true,
+    first_name VARCHAR(250) NOT NULL ,
+    middle_name VARCHAR(250) ,
+    last_name VARCHAR(250) NOT NULL,
+    personal_email VARCHAR(250) ,
+    cuny_email VARCHAR(250),
+    preferred_email boolean, /* boolean to indicate personal or cuny email contact preferrence */
+    discord VARCHAR(250),
+    emplid VARCHAR(250) NOT NULL
+)
