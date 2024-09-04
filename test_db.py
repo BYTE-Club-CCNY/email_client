9f6ffa0dbb757e4034d38b17bdf45fcd63eacc9a
joewadk <jawadk7435@gmail.com>
2024-09-04 14:06:17 -0400

    added a list of active members from previous cohort

--- test_db.py
+++ test_db.py
@@ -21,7 +21,6 @@ db = Database()
 
 #=uid = db.add(new_person)
 #db.add_cabinet(uid[0])
-db.del_cabinet(['98f04bf9-972a-46c1-9234-2129eb99492e'])
 cabinet_members = db.get_cabinet()
 print("Cabinet members:", cabinet_members)
 
