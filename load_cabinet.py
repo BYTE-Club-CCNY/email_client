276c00c8960647c93c5ce3a68efad866d16f3bdb
joewadk <jawadk7435@gmail.com>
2024-09-04 20:36:57 -0400

    added blacklist foreign key constraint

--- load_cabinet.py
+++ load_cabinet.py
@@ -2,17 +2,15 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from the .env file
+# env
 load_dotenv()
-
-# Get database credentials from environment variables
 db_host = os.getenv('POSTGRESQL_DB_HOST')
 db_user = os.getenv('POSTGRESQL_DB_USER')
 db_password = os.getenv('POSTGRESQL_DB_PASSWORD')
 db_name = os.getenv('POSTGRESQL_DB')
 db_port = os.getenv('POSTGRESQL_DB_PORT')
 
-# Connect to your PostgreSQL database
+#pgadmin
 conn = psycopg2.connect(
     host=db_host,
     user=db_user,
@@ -21,20 +19,12 @@ conn = psycopg2.connect(
     port=db_port
 )
 
-# Create a cursor object
+# this i gpt'd
 cursor = conn.cursor()
-
-# Read and execute the SQL file
 with open('database/cabinet.sql', 'r') as sql_file:
     sql_commands = sql_file.read()
-
 cursor.execute(sql_commands)
-
-# Commit changes to the database
 conn.commit()
-
-# Close the cursor and connection
 cursor.close()
 conn.close()
-
 print("SQL file executed successfully")
