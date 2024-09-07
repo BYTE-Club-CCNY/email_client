0a0ff0509a84bf576be72a29afb7239e54aac8ce
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-09-07 19:50:47 -0400

    refactored return type of validation functions

--- Validation.py
+++ Validation.py
@@ -1,6 +1,6 @@
 class Input:
     def __init__(self, message: str = None, type: any = any, nullable: bool = False):
-        from colorama import Fore, Back, Style
+        from colorama import Fore
 
         self.val = None
         self.message = Fore.BLUE + message + Fore.LIGHTWHITE_EX
@@ -14,24 +14,24 @@ class Input:
         if type is bool:
             yes_str = set(["y", "yes"])
             no_str = set(["n", "no"])
-            self.get_bool(res, yes_str, no_str)
+            self.val = self.get_bool(res, yes_str, no_str)
 
         if type is str:
-            self.get_str(res)
+            self.val = self.get_str(res)
 
         if type is int:
-            self.get_int(res)
+            self.val = self.get_int(res)
 
     def get_bool(self, res: any, yes_str: set, no_str: set):
         if res.lower() in yes_str or len(res) == 0:
-            self.val = True
+            return True
         elif res.lower() in no_str:
-            self.val = False
+            return False
         elif self.nullable and len(res) == 0:
-            self.val = True
+            return True
         else:
             res = input(self.message)
-            self.get_bool(res, yes_str, no_str)
+            return self.get_bool(res, yes_str, no_str)
 
     def get_str(self, res: str):
         if len(res) == 0 and self.nullable:
@@ -43,26 +43,26 @@ class Input:
         try:
             _ = int(res)
             res = input(self.message)
-            self.get_str(res)
+            return self.get_str(res)
         except ValueError:
-            self.val = res
+            return res
 
     def get_int(self, res: str):
         if len(res) == 0 and self.nullable:
-            self.val = None
+            return None
         if len(res) == 0:
             res = " "
         try:
             res = int(res)
-            self.val = res
+            return res
         except ValueError:
             res = input(self.message)
-            self.get_int(res)
+            return self.get_int(res)
 
 
 if __name__ == "__main__":
     try:
-        i = Input(message="test", type=bool, nullable=False)
+        i = Input(message="test", type=int, nullable=False)
         print(i.val)
     except KeyboardInterrupt:
         exit(0)
