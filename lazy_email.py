89ef0ec74a391c1860b618cb3b63da994654bc6b
joewadk <jkabir000@citymail.cuny.edu>
2025-03-09 22:14:37 -0400

    added a quick email script for a given csv file. ensure you have a body.html in root dir

---
+++ lazy_email.py
@@ -0,0 +1,59 @@
+from smtplib import SMTP_SSL
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+
+#pandas stuff
+df= pd.read_csv("<csv file here>")
+emails= df[df['Final verdict']=='Accepted']['Email Address'].tolist()
+#emails=df['Final verdict'].unique() #debug
+emails.append("acmbyte.ccny@gmail.com") #include self
+#print(emails) #debug
+
+#setup envs
+load_dotenv()
+username = os.getenv("EM_USERNAME")
+password = os.getenv("EM_PASSWORD")#ask fahad or jawad for .env
+
+#smtp config
+mailserver = "smtp.gmail.com"
+serverPort = 465
+
+
+with open("body.html", "r") as file: #make sure body.html in root. double check cdn for byte logo
+    email_body = file.read()
+
+#email structure
+subject = "Welcome to Byte!"
+recipients = emails
+
+with SMTP_SSL(mailserver, serverPort) as server:
+    try:
+        server.ehlo()
+        server.login(username, password)
+
+        for recipient in recipients:
+            mime_message = MIMEMultipart("alternative")
+            mime_message["From"] = username
+            mime_message["To"] = recipient
+            mime_message["Subject"] = subject
+            mime_message.attach(MIMEText(email_body, "plain"))
+            mime_message.attach(MIMEText(email_body, "html"))
+
+            server.sendmail(username, recipient, mime_message.as_string())
+
+        server.quit()
+        print("Emails sent successfully")
+    except smtplib.SMTPRecipientsRefused as e:
+        print("All recipient addresses refused, no one got an email", e.recipients)
+        exit(1)
+    except smtplib.SMTPHeloError as e:
+        print("Error connecting to SMTP server", e)
+        exit(1)
+    except smtplib.SMTPSenderRefused as e:
+        print("Server did not accept the sender's address", e)
+        exit(1)
